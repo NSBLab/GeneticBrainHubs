b256db9e2cb6f3327ee862568e601a3cfe33baca
Aurina <aurina.arnatk@gmail.com>
2020-09-04 16:00:15 +1000

    save as strings to avoid scientific annotation

--- code/functions/make_geneLists_enrichment.m
+++ code/functions/make_geneLists_enrichment.m
@@ -3,11 +3,19 @@ whatSets = {'POS'; 'NEG'};
 
 for i=1:length(whatSets)
     
+    % HCP
     [~, genes_ENTREZ_HCP] = extract_eQTLs('HCP', whatSets{i}); 
-    fileName = sprintf('data/reeqtls/%ssnps/HCP_%s_listGenes_onlyEntrezID.txt', whatSets{i}, whatSets{i}); 
-    dlmwrite(fileName,genes_ENTREZ_HCP)
+    genes_ENTREZ_HCP = table(string(genes_ENTREZ_HCP)); 
     
+    fileName = sprintf('data/reeqtls/%ssnps/HCP_%s_listGenes_onlyEntrezID.txt', whatSets{i}, whatSets{i}); 
+    writetable(genes_ENTREZ_HCP, fileName, 'WriteVariableNames', false)
+    
+    
+    
+    % GenCog
     [~, genes_ENTREZ_GenCog] = extract_eQTLs('GenCog', whatSets{i}); 
+    genes_ENTREZ_GenCog = table(string(genes_ENTREZ_GenCog)); 
+    
     fileName = sprintf('data/reeqtls/%ssnps/GenCog_%s_listGenes_onlyEntrezID.txt', whatSets{i}, whatSets{i}); 
     dlmwrite(fileName,genes_ENTREZ_GenCog)
     
